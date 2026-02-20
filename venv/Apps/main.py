1f720d8e4696923b74809f9be52a53a3c2fdb022
takatsune-oda <takaysune@gmail.com>
2026-02-21 00:06:37 +0900

    Blueprint導入によるコード編集

--- venv/Apps/main.py
+++ venv/Apps/main.py
@@ -1,8 +1,9 @@
 from flask import Flask, request, render_template, redirect, url_for, jsonify
 import sqlite3
-
+from routes import dp
 
 app = Flask(__name__)
+app.register_blueprint(dp)
 
 
 # データベース作成
@@ -11,45 +12,6 @@ cur = con.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS pollen(id INTEGER PRIMARY KEY AUTOINCREMENT, area_key TEXT, area_name TEXT, date DATE, pollen INT)")
 con.commit()
 con.close() 
-
-
-@app.route("/mock/pollen/<area>") # モックAPI
-def mock_pollen(area):
-    con = sqlite3.connect("pollen.db")
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute("SELECT area_key, area_name, date, pollen FROM pollen WHERE area_key = ?", (area,))
-    pollen_area = cur.fetchone()
-
-    if pollen_area:
-        return jsonify(dict(pollen_area))
-    else:
-        return jsonify({"error": "not found"}), 404
-
-
-
-@app.route("/", methods=["GET"]) # 地域選択画面
-def select_area():
-    area = request.args.get("area")
-    if area :
-        return redirect(url_for("mock_pollen", area=area))
-    else:
-        return render_template("index.html")
-
-
-@app.route("/api/areas") # 地域一覧を表示する
-def areas_list():
-    areas = []
-
-    con = sqlite3.connect("pollen.db")
-    con.row_factory = sqlite3.Row
-    cur = con.cursor()
-    cur.execute("SELECT area_key, area_name, date, pollen FROM pollen")
-    rows = cur.fetchall()
-
-    areas = [dict(row) for row in rows]
-    con.close()
-    return jsonify(areas)
 
 
 if __name__ == "__main__":
--- venv/Apps/routes.py
+++ venv/Apps/routes.py
@@ -1,2 +1,45 @@
 from flask import Blueprint, jsonify, request, render_template, redirect, url_for
+import sqlite3
 from database import get_db_connection
+
+
+dp = Blueprint("main", __name__)
+
+@dp.route("/mock/pollen/<area>") # モックAPI
+def mock_pollen(area):
+    con = get_db_connection()
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("SELECT area_key, area_name, date, pollen FROM pollen WHERE area_key = ?", (area,))
+    pollen_area = cur.fetchone()
+
+    if pollen_area:
+        return jsonify(dict(pollen_area))
+    else:
+        return jsonify({"error": "not found"}), 404
+
+
+
+@dp.route("/", methods=["GET"]) # 地域選択画面
+def select_area():
+    area = request.args.get("area")
+    if area :
+        return redirect(url_for("main.mock_pollen", area=area))
+    else:
+        return render_template("index.html")
+
+
+@dp.route("/api/areas") # 地域一覧を表示する
+def areas_list():
+    areas = []
+
+    con = sqlite3.connect("pollen.db")
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    cur.execute("SELECT area_key, area_name, date, pollen FROM pollen")
+    rows = cur.fetchall()
+
+    areas = [dict(row) for row in rows]
+    con.close()
+    return jsonify(areas)
+
